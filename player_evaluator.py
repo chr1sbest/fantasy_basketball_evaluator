2178dad66297979a4c624a8aa089b25e1ad65bd3
Chris Best <chr2sbest@gmail.com>
2014-11-12 10:50:47 -0800

    evaluator updated

--- player_evaluator.py
+++ player_evaluator.py
@@ -25,7 +25,7 @@ class Evaluator(object):
                     break
                 print "Invalid Entry. Value must be in range of -2 to 2"
             self.cats[cat] = 0 if value == "" else int(value)
-        points = {k:v for (k,v) in self.cats.iteritems() if v is not 0}
+        points = {k: v for (k, v) in self.cats.iteritems() if v is not 0}
         sure = raw_input("\n{0} \n\nCorrect? y/n\n".format(points))
         if sure in ['n', 'N', 'no', 'No']:
             self.load_categories()
@@ -44,18 +44,22 @@ class Evaluator(object):
     def evaluate(self, cats=None, player_data=None):
         """
         Evaluate player SCORE and DIFF.
-        Optionally can evaluate score with punted categories.
+        SCORE = Total sum of ((Value - Mean) / STDEV * Weight) for all cats.
+        Optionally can evaluate a subset of categories or other player_data.
         """
         cats = cats or self.cats
         player_data = player_data or self.player_data
+        # Calculate SCORE
         for player in player_data:
             totals = {x: (player[x] - self.means[x]) / self.stds[x] * \
                 cats.get(x, 0) for x in player.keys() if x in cats}
             player['SCORE'] = sum(totals.values())
+        # Re-sort according to SCORE.
         player_data.sort(key=lambda x: x['SCORE'], reverse=True)
-        for index, player in enumerate(player_data):
-            player['DIFF'] = player['RNK'] - index - 1
+        for index, player in enumerate(player_data): 
+            #Calculate Difference between ESPN rank.
             player['NEW'] = index + 1
+            player['DIFF'] = player['RNK'] - player['NEW']
         self.display(player_data=player_data)
 
     def display(self, player_data=None, *args):
@@ -66,20 +70,23 @@ class Evaluator(object):
         """
         player_data = player_data or self.player_data
         args = args or ['RNK', 'DIFF', 'SCORE']
-        base = "{0: <3} {1: <24} "
+        base = "{0: <3} {1: <24} "                  # Build base column layout
         for index, category in enumerate(args):
             base += "{" + str(index + 2) + ": <4} "
         columns = base.format('NEW', 'NAME', *args)
+        # Clear screen and display new details.
         system('clear')
         print columns
         print '\n'.join([\
             base.format(\
             player['NEW'], player['NAME'], *[player[arg] for arg in args]) \
-            for player in player_data[:self.view_limit]])
+                for player in player_data[:self.view_limit]])
 
     def punt(self, *args):
         """
         Zero out categories for a copy of player_data and re-evaluate.
+        Punting is a strategy to maximize gains and minimize losses by
+        forfeiting a few categories to focus on others.
         """
         print "Punting {0}".format(args)
         cats = {k: v for (k,v) in self.cats.iteritems() if k not in args}
@@ -89,7 +96,7 @@ class Evaluator(object):
     def sort(self, key, reverse=False):
         """
         Sort player data according to key and then display.
-        Reverse the reverse! Default displays descending now.
+        Reverse the reverse! Display descending by default.
         """
         self.player_data.sort(key=lambda x: x[key], reverse=not reverse)
         self.display()
